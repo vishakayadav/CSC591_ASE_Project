701905a2c999958ef126b4e5aeb97a20816e7870
Vishaka Yadav <vyadav@ncsu.edu>
2023-04-09 20:21:53 -0400

    [add]: setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ASE_Project",
+    version="1.0.0",
+    packages=["src", "test"],
+    license="LICENSE.md",
+    description="ASE project for Multi-objective Semi-Supervised System",
+    long_description=open("README.md", encoding="utf8").read(),
+)
