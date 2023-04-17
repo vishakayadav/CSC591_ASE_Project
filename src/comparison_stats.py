e683cdd5cfc71d324287383bd6aa04208829e7f7
Vishaka Yadav <vyadav@ncsu.edu>
2023-04-17 18:43:33 -0400

    [add]: scottknott comparison

--- src/comparison_stats.py
+++ src/comparison_stats.py
@@ -1,7 +1,12 @@
+import math
 import random
+
+import numpy as np
+import pandas as pd
 
 from src.num import NUM
 from src.utils import the
+from scipy.stats import kruskal
 
 
 def samples(t: list, n: int = None) -> dict:
@@ -65,3 +70,100 @@ def bootstrap(y0: list, z0: list) -> bool:
         if delta(i, other) > tobs:
             n += 1
     return n / the["bootstrap"] >= the["conf"]
+
+class ScottKnott():
+    def __init__(self):
+        self.rxs = []
+
+    def RX(self, t: list, s: str) -> dict:
+        t.sort()
+        return {"name": s or "", "rank": 0, "n": len(t), "show": "", "has": t}
+
+    def mid(self, t):
+        t = t["has"] if "has" in t else t
+        n = (len(t) - 1) // 2
+        return (t[n] + t[n + 1]) / 2 if len(t) % 2 == 0 else t[n + 1]
+
+    def div(self, t):
+        t = t["has"] if "has" in t else t
+        return (t[len(t) * 9 // 10] - t[len(t) * 1 // 10]) / 2.56
+
+    def merge(self, rx1, rx2):
+        rx3 = self.RX([], rx1["name"])
+        rx3["has"] = rx1["has"] + rx2["has"]
+        rx3["has"].sort()
+        rx3["n"] = len(rx3["has"])
+        return rx3
+
+    def scott_knot(self):
+        def merges(i, j):
+            out = self.RX([], self.rxs[i]["name"])
+            for k in range(i, j + 1):
+                out = self.merge(out, self.rxs[j])
+            return out
+
+        def same(lo, cut, hi):
+            l = merges(lo, cut)
+            r = merges(cut + 1, hi)
+            return cliffs_delta(l["has"], r["has"]) and bootstrap(l["has"], r["has"])
+
+        def recurse(lo, hi, rank):
+            b4 = merges(lo, hi)
+            best = 0
+            cut = None
+            for j in range(lo, hi + 1):
+                if j < hi:
+                    l = merges(lo, j)
+                    r = merges(j + 1, hi)
+                    now = (
+                        l["n"] * (self.mid(l) - self.mid(b4)) ** 2 + r["n"] * (self.mid(r) - self.mid(b4)) ** 2
+                    ) / (l["n"] + r["n"])
+                    if now > best:
+                        if abs(self.mid(l) - self.mid(r)) >= cohen:
+                            cut, best = j, now
+            if cut != None and not same(lo, cut, hi):
+                rank = recurse(lo, cut, rank) + 1
+                rank = recurse(cut + 1, hi, rank)
+            else:
+                for i in range(lo, hi + 1):
+                    self.rxs[i]["rank"] = rank
+            return rank
+
+        self.rxs.sort(key=lambda x: self.mid(x))
+        cohen = self.div(merges(0, len(self.rxs) - 1)) * the["cohen"]
+
+        recurse(0, len(self.rxs) - 1, 1)
+
+    def tiles(self):
+        huge = float("inf")
+        lo, hi = huge, float("-inf")
+        for rx in self.rxs:
+            lo, hi = min(lo, rx["has"][0]), max(hi, rx["has"][len(rx["has"]) - 1])
+        for rx in self.rxs:
+            t, u = rx["has"], []
+
+            def of(x, most):
+                return int(max(1, min(most, x)))
+
+            def at(x):
+                return t[of(len(t) * x // 1, len(t)) - 1]
+
+            def pos(x):
+                return math.floor(
+                    of(the["width"] * (x - lo) / (hi - lo + 1e-32) // 1, the["width"])
+                )
+
+            for i in range(0, the["width"] + 1):
+                u.append(" ")
+            a, b, c, d, e = at(0.1), at(0.3), at(0.5), at(0.7), at(0.9)
+            A, B, C, D, E = pos(a), pos(b), pos(c), pos(d), pos(e)
+            for i in range(A, B + 1):
+                u[i] = "-"
+            for i in range(D, E + 1):
+                u[i] = "-"
+            u[the["width"] // 2] = "|"
+            u[C] = "*"
+            x = []
+            for i in [a, b, c, d, e]:
+                x.append("%6.2f" % i)
+            rx["show"] = "".join(u) + str(x)
